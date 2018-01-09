852511d22ffd7ce3abb49e60e1bb39030812f990
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-01-10 01:19:49 +0300

    kullanıcıdan izin alındı, küçük ayarlar yapıldı uygulama bittii

--- app/src/main/java/com/emrealtunbilek/havadurumuapp/MainActivity.kt
+++ app/src/main/java/com/emrealtunbilek/havadurumuapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.emrealtunbilek.havadurumuapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -23,75 +27,46 @@ import java.util.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    var tvSehir:TextView? = null
-    var location:SimpleLocation? = null
-    var latitude:String? = null
-    var longitude:String? = null
+    var tvSehir: TextView? = null
+    var location: SimpleLocation? = null
+    var latitude: String? = null
+    var longitude: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     var spinnerAdapter=ArrayAdapter.createFromResource(this,R.array.sehirler, R.layout.spinner_tek_satir)
+        var spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.sehirler, R.layout.spinner_tek_satir)
 
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spnSehirler.background.setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP)
 
         spnSehirler.setTitle("Şehir Seçin")
         spnSehirler.setPositiveButton("SEÇ")
-        spnSehirler.adapter=spinnerAdapter
+        spnSehirler.adapter = spinnerAdapter
 
         spnSehirler.setOnItemSelectedListener(this)
 
-
-
-        if (!location!!.hasLocationEnabled()) {
-
-            //izin istenecek
-            SimpleLocation.openSettings(this);
-        }else {
-
-            location= SimpleLocation(this)
-            location?.setListener(object:SimpleLocation.Listener{
-
-                override fun onPositionChanged() {
-                    latitude = String.format("%.2f", location?.getLatitude())
-                    longitude = String.format("%.2f", location?.getLongitude())
-
-                    Log.e("WWW", ""+latitude+"  "+longitude)
-                }
-
-            })
-            // verileriGetir("Ankara")
-            oankiSehriGetir(latitude, longitude)
-
-        }
-
-
-
-
-
-
-
-
+        spnSehirler.setSelection(1)
+        verileriGetir("Ankara")
 
 
     }
 
-    private fun oankiSehriGetir(lat: String?, longt:String?) :String? {
+    private fun oankiSehriGetir(lat: String?, longt: String?) {
 
-        val url="http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+longt+"&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+        val url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longt + "&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
         var sehirAdi: String? = "Şuanki Yer"
 
-        val havaDurumuObjeRequest2 = JsonObjectRequest(Request.Method.GET, url,null, object:Response.Listener<JSONObject>{
+        val havaDurumuObjeRequest2 = JsonObjectRequest(Request.Method.GET, url, null, object : Response.Listener<JSONObject> {
 
 
             override fun onResponse(response: JSONObject?) {
 
                 var main = response?.getJSONObject("main")
-                var sicaklik= main?.getInt("temp")
-                tvSicaklik.text=sicaklik.toString()
+                var sicaklik = main?.getInt("temp")
+                tvSicaklik.text = sicaklik.toString()
 
 
 
@@ -99,14 +74,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                 tvSehir?.setText(sehirAdi)
 
 
-                var weather=response?.getJSONArray("weather")
+                var weather = response?.getJSONArray("weather")
                 var aciklama = weather?.getJSONObject(0)?.getString("description")
-                tvAciklama.text=aciklama
+                tvAciklama.text = aciklama
 
                 var icon = weather?.getJSONObject(0)?.getString("icon")
 
-                if(icon?.last() == 'd'){
-                    rootLayout.background=getDrawable(R.drawable.bg)
+                if (icon?.last() == 'd') {
+                    rootLayout.background = getDrawable(R.drawable.bg)
                     spnSehirler.background.setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP)
                     tvSehir?.setTextColor(resources.getColor(R.color.colorAccent))
                     tvAciklama.setTextColor(resources.getColor(R.color.colorAccent))
@@ -115,8 +90,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                     tvDerece.setTextColor(resources.getColor(R.color.colorAccent))
 
 
-                }else {
-                    rootLayout.background=getDrawable(R.drawable.gece)
+                } else {
+                    rootLayout.background = getDrawable(R.drawable.gece)
                     tvSehir?.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                     tvAciklama.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                     tvSicaklik.setTextColor(resources.getColor(R.color.colorPrimaryDark))
@@ -125,44 +100,28 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                     tvDerece.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                 }
 
-                var resimDosyaAdi=resources.getIdentifier("icon_"+icon?.sonKarakteriSil(),"drawable", packageName) //R.drawable.icon_50n
+                var resimDosyaAdi = resources.getIdentifier("icon_" + icon?.sonKarakteriSil(), "drawable", packageName) //R.drawable.icon_50n
                 imgHavaDurumu.setImageResource(resimDosyaAdi)
 
-                tvTarih.text=tarihYazdir()
+                tvTarih.text = tarihYazdir()
 
             }
 
 
         },
-                object : Response.ErrorListener{
+                object : Response.ErrorListener {
 
-            override fun onErrorResponse(error: VolleyError?) {
-                Log.e("VOLLEY HATA", ""+error?.printStackTrace())
-            }
+                    override fun onErrorResponse(error: VolleyError?) {
+                        Log.e("VOLLEY HATA", "" + error?.printStackTrace())
+                    }
 
-        })
+                })
 
 
         MySingleton.getInstance(this)?.addToRequestQueue(havaDurumuObjeRequest2)
 
-        if(sehirAdi != null)
-        return sehirAdi
-
-        else return "N\\A"
-
     }
 
-    override fun onResume() {
-        super.onResume()
-        location?.beginUpdates()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        location?.endUpdates();
-
-    }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
 
@@ -172,54 +131,92 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         tvSehir = view as TextView
 
-        if(position == 0){
+        if (position == 0) {
 
-            location= SimpleLocation(this)
+            location = SimpleLocation(this)
 
-            latitude=String.format("%.6f", location?.latitude)
-            longitude=String.format("%.6f", location?.longitude)
+            if(!location!!.hasLocationEnabled()){
 
-            Log.e("LAT",""+latitude)
-            Log.e("LONG",""+longitude)
+                spnSehirler.setSelection(1)
+                Toast.makeText(this, "GPS Aç ki yerini bulalım", Toast.LENGTH_LONG).show()
+                SimpleLocation.openSettings(this)
+            }else{
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),60)
+                }else{
+
+                    location = SimpleLocation(this)
+                    latitude = String.format("%.6f", location?.latitude)
+                    longitude = String.format("%.6f", location?.longitude)
+                    Log.e("LAT", "" + latitude)
+                    Log.e("LONG", "" + longitude)
+
+                    oankiSehriGetir(latitude, longitude)
+                }
 
 
-            var oankiSehirAdi = oankiSehriGetir(latitude, longitude)
-            Log.e("BURAYA GELEN SEHIR ADI:", ""+oankiSehirAdi)
-            tvSehir?.setText(oankiSehirAdi)
-        }else {
-            var secilenSehir=parent?.getItemAtPosition(position).toString()
+            }
+
+        } else {
+            var secilenSehir = parent?.getItemAtPosition(position).toString()
             tvSehir = view as TextView
             verileriGetir(secilenSehir)
         }
 
     }
 
-    fun verileriGetir(sehir:String){
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-        val url="http://api.openweathermap.org/data/2.5/weather?q="+sehir+"&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+        if(requestCode == 60){
 
-        val havaDurumuObjeRequest = JsonObjectRequest(Request.Method.GET, url,null, object:Response.Listener<JSONObject>{
+            if(grantResults.size > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                location = SimpleLocation(this)
+                latitude = String.format("%.6f", location?.latitude)
+                longitude = String.format("%.6f", location?.longitude)
+                Log.e("LAT", "" + latitude)
+                Log.e("LONG", "" + longitude)
+
+                oankiSehriGetir(latitude, longitude)
+
+            }else {
+                spnSehirler.setSelection(1)
+                Toast.makeText(this, "İzin vereydin de konumunu bulaydık :P", Toast.LENGTH_LONG).show()
+
+            }
+
+
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun verileriGetir(sehir: String) {
+
+        val url = "http://api.openweathermap.org/data/2.5/weather?q=" + sehir + "&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+
+        val havaDurumuObjeRequest = JsonObjectRequest(Request.Method.GET, url, null, object : Response.Listener<JSONObject> {
 
 
             override fun onResponse(response: JSONObject?) {
 
                 var main = response?.getJSONObject("main")
-                var sicaklik= main?.getInt("temp")
-                tvSicaklik.text=sicaklik.toString()
-
+                var sicaklik = main?.getInt("temp")
+                tvSicaklik.text = sicaklik.toString()
 
 
                 var sehirAdi = response?.getString("name")
 
 
-                var weather=response?.getJSONArray("weather")
+                var weather = response?.getJSONArray("weather")
                 var aciklama = weather?.getJSONObject(0)?.getString("description")
-                tvAciklama.text=aciklama
+                tvAciklama.text = aciklama
 
                 var icon = weather?.getJSONObject(0)?.getString("icon")
 
-                if(icon?.last() == 'd'){
-                    rootLayout.background=getDrawable(R.drawable.bg)
+                if (icon?.last() == 'd') {
+                    rootLayout.background = getDrawable(R.drawable.bg)
                     spnSehirler.background.setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP)
                     tvSehir?.setTextColor(resources.getColor(R.color.colorAccent))
                     tvAciklama.setTextColor(resources.getColor(R.color.colorAccent))
@@ -228,8 +225,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                     tvDerece.setTextColor(resources.getColor(R.color.colorAccent))
 
 
-                }else {
-                    rootLayout.background=getDrawable(R.drawable.gece)
+                } else {
+                    rootLayout.background = getDrawable(R.drawable.gece)
                     tvSehir?.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                     tvAciklama.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                     tvSicaklik.setTextColor(resources.getColor(R.color.colorPrimaryDark))
@@ -238,17 +235,16 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                     tvDerece.setTextColor(resources.getColor(R.color.colorPrimaryDark))
                 }
 
-                var resimDosyaAdi=resources.getIdentifier("icon_"+icon?.sonKarakteriSil(),"drawable", packageName) //R.drawable.icon_50n
+                var resimDosyaAdi = resources.getIdentifier("icon_" + icon?.sonKarakteriSil(), "drawable", packageName) //R.drawable.icon_50n
                 imgHavaDurumu.setImageResource(resimDosyaAdi)
 
-                tvTarih.text=tarihYazdir()
-
+                tvTarih.text = tarihYazdir()
 
 
             }
 
 
-        }, object : Response.ErrorListener{
+        }, object : Response.ErrorListener {
 
             override fun onErrorResponse(error: VolleyError?) {
 
@@ -260,7 +256,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         MySingleton.getInstance(this)?.addToRequestQueue(havaDurumuObjeRequest)
     }
 
-    fun tarihYazdir():String{
+    fun tarihYazdir(): String {
 
         var takvim = Calendar.getInstance().time
         var formatlayici = SimpleDateFormat("EEE, MMM yyyy", Locale("tr"))
@@ -269,12 +265,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         return tarih
 
 
-
-
     }
 }
 
 private fun String.sonKarakteriSil(): String {
     //50n ifadeyi 50 olarak geriye yollar
-    return this.substring(0, this.length-1)
+    return this.substring(0, this.length - 1)
 }
